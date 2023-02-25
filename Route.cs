945c3bf753605c811f31e7ed18f938beba2396eb
Daniel1060 <danielohara99@outlook.com>
2023-02-25 23:53:54 +0000

    edit

--- Route.cs
+++ Route.cs
@@ -3,29 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace RoutePlanner
+namespace Route_Planner
 {
-  internal class Route
-  {
-    private Ship ship;
-    private List<Job> destinations = new List<Job>();
+    internal class Route
+    {
+        private Ship ship;
+        private List<Job> jobs = new List<Job>();
+        private List<Job> tempJob = new();
 
-    public Route(Ship ship, List<Job> destinations){
-      this.ship = ship;
-      this.destinations = destinations;
-    }
-
-    public void calculateRoute(Job location ,List<Job> stops){
-      if(stops.length > 1){
-        int i = 0;
-        for(Job stop in stops){
-          List<Job> tmp = stops;
-          tmp = tmp.RemoveAt(i);
-          this.calculateRoute(stops[i].location, tmp);
-          i++;
+        public Route(Ship ship, List<Job> jobs)
+        {
+            this.ship = ship;
+            this.jobs = jobs;
         }
-      }
+
+        public void calculateRoute(Port location, List<Job> jobs)
+        {
+            if (jobs.Count > 1)
+            {
+                int i = 0;
+        foreach(Job job in jobs)
+                {
+                    List<Job> tmp = jobs;
+                    tmp.Remove(job);
+                    this.calculateRoute(job.destination, tmp);
+                    i++;
+                    MessageBox.Show($"Iterating {i}");
+                }
+            }
+        }
+
+        internal void calculateEfficientRoute()
+        {
+            calculateRoute(jobs[0].destination, jobs);
+        }
     }
-  }
 }
